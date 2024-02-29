c084e584d9b9a5b55a56a8bd8f4bf97e90f382cf
AliciaNazar <alicianazarab@gmail.com>
2024-02-29 00:02:49 -0300

    background-color del div cambia segun extension de la lista de tareas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, OnInit, ViewChild, inject} from '@angular/core';
+import { Component, OnInit, inject} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { TareasService } from './services/tareas.service';
@@ -14,46 +14,44 @@ import { CommonModule } from '@angular/common';
 
 export class AppComponent implements OnInit{
 
-  
   listaDeTareas: string[]=[];
   nuevaTarea: string="";
-
-  height:number=0;
-  valorColor: string = "";
   miColor: string="";
-
-  @ViewChild('div1', { static: false }) div1!: ElementRef;
-
-  @HostListener('window:resize',['$event'])
-    onDivResize(event: Event) {
-      this.height = this.div1.nativeElement.offsetHeight;
-      // console.log("Nueva altura del div: ", this.height);
-      const opacity = this.funcionOpacidad(this.height);
-      // console.log("opacidad: ", opacity);
-      this.miColor = `rgba(63, 167, 164, ${opacity})`;
-    }
-
-    funcionOpacidad(height: number): number {
-      const minHeight = 300;
-      const maxHeight = 2000;
-      const opacity = (height - minHeight) / (maxHeight - minHeight);
-      return Math.max(0, Math.min(1, opacity)); //para asegurar que la opacidad esté dentro del rango [0, 1]
-    }
-
 
   private _tareasService = inject(TareasService)
 
   ngOnInit(): void {
     this.listaDeTareas=this._tareasService.getTareas();
+    this.onListResize();
+  }
+
+  onListResize() {
+    const height = this.listaDeTareas.length * 40; 
+    console.log("height es: "+height);
+    const opacity = this.funcionOpacidad(height);
+    console.log("la opacidad es: "+opacity);
+    if (opacity<=0.083){
+      this.miColor = `rgba(63, 167, 164, 0.083)`;
+    }else{
+      this.miColor = `rgba(63, 167, 164, ${opacity})`;
+    }
   }
 
   agregarTarea(){
     this._tareasService.agregarTarea(this.nuevaTarea);
     this.nuevaTarea="";
     this.listaDeTareas=this._tareasService.getTareas();
+    this.onListResize();
   }
   eliminarTarea(indice: number){
     this._tareasService.eliminarTarea(indice);
     this.listaDeTareas=this._tareasService.getTareas();
+    this.onListResize();
+  }
+  funcionOpacidad(height: number): number {
+    const minHeight = 300;
+    const maxHeight = 1500;
+    const opacity = (height - minHeight) / (maxHeight - minHeight);
+    return Math.max(0, Math.min(1, opacity)); //para asegurar que la opacidad esté dentro del rango [0, 1]
   }
 }
